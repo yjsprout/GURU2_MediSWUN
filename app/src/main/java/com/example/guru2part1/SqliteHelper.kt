4efb448eec7bea472e82cf29e432260ce0fb041a
yjsprout <ilbnmyj23@gmail.com>
2023-01-31 16:24:27 +0900

    git commit

--- app/src/main/java/com/example/guru2part1/SqliteHelper.kt
+++ app/src/main/java/com/example/guru2part1/SqliteHelper.kt
@@ -4,8 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-//https://inistory.tistory.com/118 공부
-//https://itmoon.tistory.com/45 코드 참고
+
 // SQLiteOpenHelper 상속받아 SQLite 를 사용하도록 하겠습니다.
 class SqliteHelper(context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper(context, name, factory, version) {
 
@@ -42,54 +41,4 @@ class SqliteHelper(context: Context?, name: String?, factory: SQLiteDatabase.Cur
         wd.close()
     }
 
-    //select 메소드
-    /*fun selectMemo():MutableList<MediClass>{
-        val list = mutableListOf<MediClass>()
-        //전체조회
-        val selectAll = "select * from medicine"
-        //읽기전용 데이터베이스 변수
-        val rd = readableDatabase
-        //데이터를 받아 줍니다.
-        val cursor = rd.rawQuery(selectAll,null)
-
-        //반복문을 사용하여 list 에 데이터를 넘겨 줍시다.
-        while(cursor.moveToNext()){
-            val id = cursor.getLong(cursor.getColumnIndexOrThrow("id"))
-            val mdname = cursor.getString(cursor.getColumnIndexOrThrow("mdname"))
-            val content = cursor.getString(cursor.getColumnIndexOrThrow("content"))
-            val datetime = cursor.getString(cursor.getColumnIndexOrThrow("datetime"))
-
-            list.add(MediClass(id, mdname,datetime,content))
-        }
-        cursor.close()
-        rd.close()
-
-        return list
-    }*/
-
-    //update 메소드 - 수정하는 거 구현하려면 어떤 식으로?
-    /*fun updateMemo(medicine: MediClass){
-        val values = ContentValues()
-
-        values.put("mdname",medicine.mdname)
-        values.put("datetime",medicine.datetime)
-        values.put("content",medicine.content)
-
-        val wd = writableDatabase
-        wd.update("medicine",values,"id=${medicine.id}",null)
-        wd.close()
-
-    }*/
-
-    /*//delete 메소드
-    fun deleteMemo(medicine: MediClass){
-        val delete = "delete from mediTBL where id = ${medicine.id}"
-        val db = writableDatabase
-        db.execSQL(delete)
-        db.close()
-
-    }*/
-
 }
-
-//val startDate = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(it.first)
